280b52a7e515168e57c3565edf520afed5e78cba
Josjiang <1135719658@qq.com>
2025-04-01 10:50:41 +0800

    add twoway data binding

---
+++ GeishaConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using yuelinLib;
+
+var vm = new ViewModel();
+var view = new View();
+view.Bind(vm);
+
+// View => ViewMode
+Console.WriteLine("Input Content:");
+
+vm.Text = Console.ReadLine();
+
+// ViewModel => View
+vm.Text = "Hello, C#!";
---
+++ yuelinLib/Observable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace yuelinLib
+{
+    public class Observable
+    {
+        private readonly Dictionary<string, List<Action<object>>> _subscribers = new Dictionary<string, List<Action<object>>>();
+
+        public void SetProperty<T>(string propertyName, ref T field, T newValue)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                Notify(propertyName, newValue);
+            }
+        }
+
+        public void Subscribe<T>(string propertyName, Action<object, T> callback)
+        {
+            if (!_subscribers.ContainsKey(propertyName))
+            {
+                _subscribers[propertyName] = new List<Action<object>>();
+            }
+            _subscribers[propertyName].Add((value) => callback(this, (T)value));
+        }
+
+        private void Notify(string propertyName, object value)
+        {
+            if (_subscribers.TryGetValue(propertyName, out var callbacks))
+            {
+                foreach (var callback in callbacks)
+                {
+                    callback(value);
+                }
+            }
+        }
+    }
+
+    public class ViewModel : Observable
+    {
+        private string _text = "default";
+
+        public string Text
+        {
+            get => _text;
+            set => SetProperty(nameof(Text), ref _text, value);
+        }
+    }
+
+    public class View
+    {
+        public void Bind(ViewModel vm)
+        {
+            vm.Subscribe<string>(nameof(ViewModel.Text), (sender, value) =>
+            {
+                Console.WriteLine($"Text changed to {value}");
+            });
+        }
+    }
+}
